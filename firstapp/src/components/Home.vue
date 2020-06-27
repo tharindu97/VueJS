e5f6aede88b480d88329acdc0a570c051c806c35
tharindu97 <tharindukavishna@gmail.com>
2020-06-27 11:48:05 +0530

    create_navbar

---
+++ firstapp/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <form class="form-horizontal">
+                <fieldset>
+                    <legend>Music App</legend>
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-lg-2 control-label">Search music</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="search here">
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                Result
+            </div>
+        </div>
+    </div>
+</template>
