a19073dd9eae3f54c6f7574fafae60d796f89d94
tharindu97 <tharindukavishna@gmail.com>
2020-06-28 19:25:11 +0530

    api_call

--- firstapp/src/components/Home.vue
+++ firstapp/src/components/Home.vue
@@ -4,19 +4,31 @@
             <form class="form-horizontal">
                 <fieldset>
                     <legend>Music App</legend>
-                    <div class="form-group">
-                        <label for="inputEmail" class="col-lg-2 control-label">Search music</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" id="inputEmail" placeholder="search here">
-                        </div>
-                    </div>
+                    <app-search></app-search>
                 </fieldset>
             </form>
         </div>
         <div class="row">
             <div class="col-md-12">
-                Result
+               <app-result></app-result>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+
+    import Search from './Search';
+    import Result from './Result';
+
+    export default {
+
+        components:{
+
+            'app-result': Result,
+            'app-search': Search
+
+        }
+    }
+
+</script>
---
+++ firstapp/src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+    
+     <div class="form-group">
+        <label for="inputEmail" class="col-lg-2 control-label">Search music</label>
+        <div class="col-lg-10">
+            <input type="text" class="form-control" id="inputEmail" placeholder="search here" @input="keyPressed">
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    import axios from 'axios'
+    export default {
+
+        methods:{
+
+            keyPressed(event){
+                var key = event.target.value
+                
+                axios.get('')
+                .then(responce => {
+                    console.log(responce.data)
+                })
+                .catch(e=>{
+                    this.error.push(e);
+                })
+            }
+
+        }
+        
+    }
+
+</script>
